5ac92f1c02aa8a49363cfd560615d42a9d04f31c
TP31730273 <raju31730273@gmail.com>
2022-01-05 00:24:17 +0530

    added db

---
+++ todoapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from todoapp.models import *
+admin.site.register(Master)
+admin.site.register(Department)
+admin.site.register(Profile)
+admin.site.register(connection)
+admin.site.register(connectionRequet)
+admin.site.register(ToDoMember)
+admin.site.register(ToDOparticipant)
