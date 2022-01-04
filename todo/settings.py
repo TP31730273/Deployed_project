11571516e247031743ba9efe5874980dc14f4d3c
TP31730273 <raju31730273@gmail.com>
2022-01-05 00:36:13 +0530

    a db

--- todo/settings.py
+++ todo/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'CONN_MAX_AGE': 500,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'CONN_MAX_AGE': 500,
     }
 }
 import dj_database_url
